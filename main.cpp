2c75c97430eba177158bb76a8aa155bc9c8ddcad
AmmarAbozaid <ammarabozaid07@gmail.com>
2025-04-16 20:02:20 +0200

    Update Enctyption.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
-# include <iostream <
- # include <fstream <
- # include <string <
- # include <vector <
+# include <iostream>
+ # include <fstream>
+ # include <string>
+ # include <vector>
  using namespace std ;
- string xorEncryptDecrypt(const string&
-text, char key)
- }
+ string xorEncryptDecrypt(const string& text, char key)
+{
     string result = text ;
     for (char & c : result)
-    }
+     {
         c ^= key ;
-    {
+}
     return result ;
- {
- int main ) (
- }
+    }
+ int main ()
+{
     string filename = "input.txt " ;
     string encryptedFile = "encrypted.txt " ;
     string decryptedFile = "decrypted.txt " ;
@@ -22,50 +21,50 @@ text, char key)
 
     ifstream inFile(filename) ;
     if (!inFile)
-    }
+    {
         cerr << "Error opening input file!" <<endl ;
         return 1 ;
-    {
+}
     string text ;
     string line ;
     while (getline(inFile, line))
-    }
+     {
         text += line + "\n " ;
-    {
-    inFile.close ) ( ;
+}
+    inFile.close () ;
     string encryptedText =
     xorEncryptDecrypt(text, key) ;
     ofstream outFile(encryptedFile) ;
     if (!outFile)
-    }
+    {
         cerr<< "Error opening encryption file!" << endl ;
         return 1 ;
-    {
+}
     outFile << encryptedText ;
     (outFile.close )  ;
     cout << "The text is encrypted andsaved in " << encryptedFile << endl ;
 
     ifstream encFile(encryptedFile) ;
     if (!encFile)
-    }
+    {
         cerr << " Error opening encryptionfile!" << endl ;
         return 1 ;
-    {
+}
     string encText ;
     while (getline(encFile, line))
-    }
+     {
         encText += line + "\n " ;
-    {
+}
         (encFile.close )  ;
 
     string decryptedText =xorEncryptDecrypt(encText, key) ;
 
     ofstream decFile(decryptedFile) ;
     if (!decFile)
-    }
+    {
         cerr << "Error opening decryptionfile!" << endl ;
         return 1 ;
-    {
+}
     decFile << decryptedText ;decFile.close ) ( ;
     cout << "Decrypted and original textsaved in " << decryptedFile << endl ;
     return 0 ;
